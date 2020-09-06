75a07a535b182cd1944c866cf8a005dd41602b0c
shuxinhuan <vlanxin11@gmail.com>
2020-09-06 14:41:48 +0800

    create gh-pages

---
+++ precache-manifest.8be8162f9dd4b79643b4780685f57bbd.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffdeee96dfe38188ab6b480f8748718e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2c67cec65e11859fef4d",
+    "url": "./static/css/main.61f28b3c.chunk.css"
+  },
+  {
+    "revision": "0c60bf079788ac411057",
+    "url": "./static/js/2.310b1df2.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "./static/js/2.310b1df2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c67cec65e11859fef4d",
+    "url": "./static/js/main.c901ed60.chunk.js"
+  },
+  {
+    "revision": "3234c4f5edc2aef534f8",
+    "url": "./static/js/runtime-main.728c8226.js"
+  },
+  {
+    "revision": "409c7f79a42e56c785f50ed37535f0be",
+    "url": "./static/media/Montserrat-Light.409c7f79.ttf"
+  },
+  {
+    "revision": "cbdf3ab950d15019fff93460614948b7",
+    "url": "./static/media/Montserrat-SemiBold.cbdf3ab9.otf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "./static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "10aa1d8fc05e49c01be462cab49e5e7f",
+    "url": "./static/media/bigTitle.10aa1d8f.woff"
+  },
+  {
+    "revision": "64ea21eaa904e8c90d73cbb84df6b415",
+    "url": "./static/media/sample.64ea21ea.gif"
+  }
+]);
